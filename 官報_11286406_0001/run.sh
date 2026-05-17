0c0851a91289469e54d3211b0acf78a7a0969fc3
qqwwee0409 <sazawa84@gmail.com>
2026-05-18 08:13:22 +0900

    wip

--- 官報_11286406_0001/run.sh
+++ 官報_11286406_0001/run.sh
@@ -1,3 +1,8 @@
 #
-miniocr NDL11286406_官報_1946年11月16日（号外）.pdf --toml ./config.toml --end 1 --ja_only
+# miniocr NDL11286406_官報_1946年11月16日（号外）.pdf --toml ./config.toml --end 1 --ja_only
 # miniocr NDL11286406_官報_1946年11月16日（号外）.pdf --end 1 --ja_only
+
+# miniocr NDL11286406_官報_1946年11月16日（号外）.pdf --toml ./config2.toml \
+#     --start 2 --end 2 --ja_only $*
+
+miniocr jpg/0002_0000.jpg --ja_only $*
