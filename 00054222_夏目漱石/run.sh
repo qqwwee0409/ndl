fffee84832a1815eb52be6f5a3ce460d6b01494a
qqwwee0409 <sazawa84@gmail.com>
2026-05-20 14:50:15 +0900

    wip

---
+++ 00054222_夏目漱石/run.sh
@@ -0,0 +1,9 @@
+#
+# ndl=1883179
+# miniocr 漱石全集_$ndl --begin 0010_0000 --end 0019_0000 --output $ndl --ychar $*
+
+# ndl=1883288
+# miniocr 漱石全集_$ndl --begin 0011_0000 --end 0020_0000 --output $ndl --ychar 16 $*
+
+ndl=1888306
+miniocr 漱石全集_$ndl --begin 0016_0000 --end 0020_0000 --output $ndl --ychar15  $*
