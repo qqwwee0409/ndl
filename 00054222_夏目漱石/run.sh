ce54220101790c0b10ee126221911bd4e941684b
qqwwee0409 <sazawa84@gmail.com>
2026-05-24 09:31:44 +0900

    wip

--- 00054222_夏目漱石/run.sh
+++ 00054222_夏目漱石/run.sh
@@ -4,11 +4,14 @@ param=" --ychar $ychar --choon"
 # ndl=1883179
 # ocr 漱石全集_$ndl --begin 0020_0000 --end 0030_0000 --output $ndl $param $*
 
-ndl=1883187
-ocr 漱石全集_$ndl --begin 0191_0000 --end 0200_0000 --output $ndl  $param $*
+# ndl=1883187
+# ocr 漱石全集_$ndl --begin 0231_0000 --end 0240_0000 --output $ndl  $param $*
 
-# ndl=1883288
-# ocr 漱石全集_$ndl --begin 0031_0000 --end 0040_0000 --output $ndl $param $*
+ndl=1883288
+ocr 漱石全集_$ndl --begin 0041_0000 --end 0050_0000 --output $ndl $param $*
 
 # ndl=1883306
-# ocr 漱石全集_$ndl --begin 0051_0000 --end 0060_0000 --output $ndl $param $*
+# ocr 漱石全集_$ndl --begin 0061_0000 --end 0070_0000 --output $ndl $param $*
+
+# ndl=1883350
+# ocr 漱石全集_$ndl --begin 0011_0000 --end 0020_0000 --output $ndl $param $*
