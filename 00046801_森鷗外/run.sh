1c96a95894db866e4c8ed74dd4e33d9fbef89172
qqwwee0409 <sazawa84@gmail.com>
2026-05-26 07:37:45 +0900

    wip

---
+++ 00046801_森鷗外/run.sh
@@ -0,0 +1,3 @@
+#
+arcid="moriogaishu00moriuoft"
+ocr ${arcid}_jp2 --begin ${arcid}_0505 --end ${arcid}_0507 --output $arcid --toml ./config.toml --ja_only
--- 00054222_夏目漱石/run.sh
+++ 00054222_夏目漱石/run.sh
@@ -2,8 +2,8 @@
 ychar=12
 param=" --ychar $ychar --choon"
 
-# ndl=1883179
-# ocr 漱石全集_$ndl --begin 0061_0000 --end 0070_0000 --output $ndl $param $*
+ndl=1883179
+ocr 漱石全集_$ndl --begin 0071_0000 --end 0080_0000 --output $ndl $param $*
 
 # ndl=1883187
 # ocr 漱石全集_$ndl --begin 0231_0000 --end 0240_0000 --output $ndl  $param $*
@@ -11,8 +11,8 @@ param=" --ychar $ychar --choon"
 # ndl=1883288
 # ocr 漱石全集_$ndl --begin 0041_0000 --end 0050_0000 --output $ndl $param $*
 
-ndl=1883306
-ocr 漱石全集_$ndl --begin 0091_0000 --end 0100_0000 --output $ndl $param $*
+# ndl=1883306
+# ocr 漱石全集_$ndl --begin 0091_0000 --end 0100_0000 --output $ndl $param $*
 
 # ndl=1883318
 # ocr 漱石全集_$ndl --begin 0013_0000 --end 0020_0000 --output $ndl $param $*
